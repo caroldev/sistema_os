8c9657da38f4d1b03bb464bd46db8185f24f6378
Carol Pereira <caroldev01@gmail.com>
2017-04-19 17:07:28 -0300

    1st commit

---
+++ src/br/com/descolanerd/dal/ModuloConexao.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.descolanerd.dal;
+
+import java.sql.*;
+
+/**
+ *
+ * @author Rodolfo
+ */
+public class ModuloConexao {
+
+    public static Connection conector() {
+        java.sql.Connection conexao = null;
+        String driver = "com.mysql.jdbc.Driver";
+        String url = "jdbc:mysql://localhost:3360/sistemaos";
+        String user = "root";
+        String password = "";
+        try {
+            Class.forName(driver);
+            conexao = DriverManager.getConnection(url, user, password);
+            return conexao;
+        } catch (Exception e) {
+            return null;
+        }
+
+    }
+}
